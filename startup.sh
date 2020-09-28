c167930b88a1a5700dd00d9b3d0862eebf4b231d
pianomanx <2634655+pianomanx@users.noreply.github.com>
2020-09-27 21:52:23 -0500

    Update startup.sh
    just minor spelling. nothing impactful.
    
    #!/bin/ash > #!/bin/bash

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 sed -in 's/#LoadModule proxy_module/LoadModule proxy_module/' /usr/local/apache2/conf/httpd.conf
 sed -in 's/#LoadModule proxy_http_module/LoadModule proxy_http_module/' /usr/local/apache2/conf/httpd.conf
 sed -in 's/#LoadModule ssl_module/LoadModule ssl_module/g' /usr/local/apache2/conf/httpd.conf
